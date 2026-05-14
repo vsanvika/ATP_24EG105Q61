85ca9927aa9efe2de511ad2eb6465c2a16f50a39
vsanvika <24eg105q61@anurag.edu.in>
2026-05-14 19:14:41 +0530

    initial commit for weekly assignments

--- WEEK2/StudentPerformanceDashboard.js
+++ WEEK2/StudentPerformanceDashboard.js
@@ -2,92 +2,39 @@
 
 // Array containing student details
 const students = [
-
-  // Student 1
   { id: 1, name: "Ravi", marks: 78 },
-
-  // Student 2
   { id: 2, name: "Anjali", marks: 92 },
-
-  // Student 3
   { id: 3, name: "Kiran", marks: 35 },
-
-  // Student 4
   { id: 4, name: "Sneha", marks: 88 },
-
-  // Student 5
   { id: 5, name: "Arjun", marks: 40 }
 ];
-
-
-// ---------------------------------------------------
-// FILTER METHOD
 // Filter students who passed (marks >= 40)
-// ---------------------------------------------------
-
-let filtermark = students.filter(
-  stuObj => stuObj.marks >= 40
-)
-
+let filtermark = students.filter(stuObj => stuObj.marks >= 40)
 // Display passed students
 console.log(filtermark)
-
-
-// ---------------------------------------------------
-// MAP METHOD
 // Assign grades based on student marks
-// ---------------------------------------------------
-
 let grade = students.map(element => {
-
   // Grade A for marks >= 90
   if (element.marks >= 90)
-
     return "A"
-
   // Grade B for marks >= 75
   else if (element.marks >= 75)
-
     return "B"
-
   // Grade C for marks >= 60
   else if (element.marks >= 60)
-
     return "c"
-
   // Grade D for marks below 60
   else
-
     return "D"
 }
 )
-
 // Display grades
 console.log("grade:", grade)
-
-
-// ---------------------------------------------------
-// REDUCE METHOD
 // Calculate average marks of all students
-// ---------------------------------------------------
-
-const averageMarks = students.reduce(
-
-  (sum, student) => sum + student.marks,
-
-  0
-
-) / students.length;
-
+const averageMarks = students.reduce((sum, student) => sum + student.marks,0) / students.length;
 // Display average marks
 console.log(averageMarks);
-
-
-// ---------------------------------------------------
-// FIND METHOD
 // Find student whose marks are 92
-// ---------------------------------------------------
-
 let find = students.find(
   mark => mark.marks == 92)
 // Display student details
@@ -96,5 +43,5 @@ console.log(find)
 let findindex = students.findIndex(
   element => element.name === "Kiran"
 )
-
 // Display indexconsole.log(findindex)
+console.log(findindex)
