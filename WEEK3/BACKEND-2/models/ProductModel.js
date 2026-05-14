874c61673a33884e3d3f9af4235d193c1309fa7d
vsanvika <24eg105q61@anurag.edu.in>
2026-05-14 20:26:13 +0530

    initial commit for weekly assignments

--- WEEK3/BACKEND-2/models/ProductModel.js
+++ WEEK3/BACKEND-2/models/ProductModel.js
@@ -1,24 +1,49 @@
-import { Schema,model } from "mongoose";
-const productSchema=new Schema({
-    productId:{
-        type:Number,
-        required:[true,"productId is required"],
+// Import Schema and model from mongoose
+import { Schema, model } from "mongoose";
+// Create Product Schema
+// Schema defines structure of product document
+const productSchema = new Schema(
+    {
+        // Product ID Field
+        productId: {
+            // Data type should be Number
+            type: Number,
+            // Field is mandatory
+            required: [true, "productId is required"],
+        },
+        // Product Name Field
+     productName: {
+            // Data type should be String
+            type: String,
+            // Field is mandatory
+            required: [true, "productName is required"],
+        },
+        // Price Field
+        price: {
+            // Data type should be Number
+            type: Number,
+            // Field is mandatory
+            required: [true, "price is required"],
+            // Minimum allowed price
+            min: [10000, "min price at least 10000"],
+            // Maximum allowed price
+            max: [50000, "max price at most 50000"],
+        },
+        // Brand Field
+        brand: {
+            // Data type should be String
+            type: String,
+        },
     },
-    productName:{
-        type:String,
-        required:[true,"productName is required"],
-    },
-    price:{
-      type:Number,
-      required:[true,"price is required"],
-      min:[10000,"min price at least 10000"],
-      max:[50000,"max price atmost 50000"],
-    },
-    brand:{
-        type:String,
-    },
-},{
-    versionKey:false,
-    timestamps:true,
-})
-export const ProductModel=model("product",productSchema)
+    // Schema Configuration Options
+    {
+        // Disable __v field
+        versionKey: false,
+        // Automatically add createdAt & updatedAt fields
+        timestamps: true,
+    }
+)
+// Generate Product Model
+// "product" → collection name
+// productSchema → schema structure
+export const ProductModel = model("product",productSchema)
